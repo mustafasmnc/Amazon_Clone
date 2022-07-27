b0f1433ce89f384511ea7017eee0a0b3cf8701fc
Mustafa <mustafasmnc@gmail.com>
2022-07-27 19:57:47 +0300

    Connecting to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,16 +1,26 @@
 // imports packages
 const express = require('express')
 const mongoose = require('mongoose')
+require('dotenv').config()
+
 
 // import from other files
 const authRouter = require('./routes/auth')
 
 // init
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const app = express()
 
 // middleware
 app.use(authRouter)
+
+// mongodb connections
+const DB = process.env.MONGO_DB; //Mongodb link
+mongoose.connect(DB).then(() => {
+    console.log('Connected to MongoDB')
+}).catch((err) => {
+    console.log(`Error: ${err}`)
+})
 
 app.listen(PORT, () => {
     console.log(`Connected to ${PORT}`)
