9984c8d8d54d0d842014ad0e33b9d49e0dad237f
Mustafa <mustafasmnc@gmail.com>
2022-07-28 22:30:13 +0300

    Creating SignIn Route And Connect with ClientSide

--- lib/router.dart
+++ lib/router.dart
@@ -1,10 +1,13 @@
 import 'package:amazon_clone/features/auth/screens/auth_screen.dart';
+import 'package:amazon_clone/features/home/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 Route<dynamic> generateRoute(RouteSettings routeSettings) {
   switch (routeSettings.name) {
     case AuthScreen.routeName:
       return MaterialPageRoute(builder: (_) => AuthScreen());
+    case HomeScreen.routeName:
+      return MaterialPageRoute(builder: (_) => HomeScreen());
     default:
       return MaterialPageRoute(
           builder: (_) => Scaffold(
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const User = require('../models/user')
 const bcryptjs = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const authRouter = express.Router()
 
-// sign up
+// sign up route
 authRouter.post('/api/signup', async (req, res) => {
     try {
         const { name, email, password } = req.body
@@ -17,12 +18,36 @@ authRouter.post('/api/signup', async (req, res) => {
         const hashedPassword = await bcryptjs.hash(password, 8)
 
         let user = new User({
-            email, 
-            password: hashedPassword, 
+            email,
+            password: hashedPassword,
             name,
         })
         user = await user.save()
         res.json(user)
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+
+})
+
+// sign in route
+authRouter.post('/api/signin', async (req, res) => {
+    try {
+        const { email, password } = req.body
+
+        const existingUser = await User.findOne({ email })
+        if (!existingUser) {
+            return res.status(400).json({ msg: 'User with this email does not exists!' })
+        }
+
+        const isMatchPassword = await bcryptjs.compare(password, existingUser.password)
+        if (!isMatchPassword) {
+            return res.status(400).json({ msg: 'Incorrect password!' })
+        }
+
+        const token = jwt.sign({ id: existingUser._id }, "passwordKey")
+        res.json({ token, ...existingUser._doc })
+
     } catch (error) {
         res.status(500).json({ error: error.message })
     }
